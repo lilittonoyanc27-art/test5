bb136d8c3699a7b1d34a1967b82c67cff265412d
lilittonoyanc27-art <lilit.tonoyanc27@gmail.com>
2026-04-18 09:57:45 +0400

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SeaBattleGame from './SeaBattleGame';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-slate-950">
+      <SeaBattleGame />
+    </div>
+  );
+}
